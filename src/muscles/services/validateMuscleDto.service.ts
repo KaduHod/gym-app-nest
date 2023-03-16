75b06bb8f6b405e731bb97c1bdb7b61250486faf
carlos.ribas <kaduhod@IESDE-5241.iesde.local>
2023-03-16 17:59:20 -0300

    finish

---
+++ src/muscles/services/validateMuscleDto.service.ts
@@ -0,0 +1,18 @@
+import { MuscleGroupE } from "src/domain/entitys";
+import QueryMuscleGroupDto from "../muscle.validator";
+
+export default class ValidateMuscleGroupQueryDto {
+    private dto: QueryMuscleGroupDto
+    private args: MuscleGroupE | Partial<MuscleGroupE>
+    constructor(){}
+
+    setArgs(args:MuscleGroupE | Partial<MuscleGroupE>): this{
+        this.args = args
+        return this
+    }
+
+    setDto(dto:QueryMuscleGroupDto): this{
+        this.dto = dto 
+        return this
+    }
+}
