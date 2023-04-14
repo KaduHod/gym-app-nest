346ab0fe3b0b8d7d9eff3ce552b73d4b7891a5ad
carlos.ribas <kaduhod@IESDE-5241.iesde.local>
2023-04-14 15:33:42 -0300

    prisma error filter

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import GlobalErrorHandler from './errors/Global.errors';
+import { PrismaExceptionFilter } from './errors/prisma.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new GlobalErrorHandler());
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new PrismaExceptionFilter())
   await app.listen(3000);
 }
 bootstrap();
