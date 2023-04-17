b3c9b1065559cb1888ad606fa5b2251ed5453e8b
carlos.ribas <kaduhod@IESDE-5241.iesde.local>
2023-04-17 10:04:55 -0300

    ups

--- src/data-source.config.ts
+++ src/data-source.config.ts
@@ -5,19 +5,23 @@ import { Dobrascutaneas } from './entitys/Dobrascutaneas.entity';
 import { Medidas } from './entitys/Medidas.entity';
 import { Personal } from './entitys/Personais.entity';
 import { User } from './entitys/Users.entity';
+import * as env from 'dotenv'
+env.config()
 
 export const config: DataSourceOptions = {
-    type: 'mysql',
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "123456",
-    database: "gymapp2",
+    type: "mysql",
+    host: process.env.DATABASE_HOST,
+    port: Number(process.env.DATABASE_PORT),
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PWD,
+    database: process.env.GYM_DATABASE_NAME,
     migrationsRun: true,
     synchronize: false,
     migrations: ['dist/database/migrations/*.js'],
     entities: ['dist/**/*.entity.js'],
 } 
+
+console.log(config)
 
 const dataSource = new DataSource(config);
 
