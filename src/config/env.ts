45bf95eb9492ee10cfba9d175ea3d0337d522d39
Carlos <carlosjr.ribas@gmail.com>
2023-05-20 22:46:30 -0300

    env

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,7 +3,7 @@ dotenv.config()
 
 export const getEnv = () => {
     const stage = process.env.ENVIROMENT
-    const regex = new RegExp(`${stage}_`)
+    const regex = new RegExp(`^${stage}_.*`)
     const keys = Object.keys(process.env).filter(key => key.match(regex))
     return keys.reduce((acc, key) => {
         const newKey = key.split(`${stage}_`)[1]
