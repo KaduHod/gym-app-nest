e169e4467b81c000f9b7c2cc4d17290905ddf985
carlos.ribas <carlos.ribas@iesde.com.br>
2023-06-14 12:01:31 -0300

    styles

--- public/js/menu.js
+++ public/js/menu.js
@@ -4,19 +4,19 @@ const menuItems = document.getElementById('list-menu');
 const closeMenu = document.getElementById('menu-close');
 const menu = document.getElementById('menu-hamburguer');
 const bcMenu = document.getElementById('background-menu');
-
-menu.onclick = function() {
-    toggleMenuIcon();
-    toggle(menuItems, ['-translate-x', 'opacity-0']);
-}
-
-closeMenu.onclick = function(){
-    toggleMenuIcon();
-    toggle(menuItems, ["-translate-x", 'opacity-0']);
-}
+const lisMenu = [...document.getElementsByClassName('menu-li')]
 
 const toggleMenuIcon = () => {
     toggle(menu, ['hidden']);
     toggle(closeMenu, ['hidden']);
     toggle(bcMenu, ['border-red-950','border-red-800','shadow-2xl']);
 }
+
+const toggleMenuMain = () => {
+    toggleMenuIcon();
+    toggle(menuItems, ["-translate-x-full-full"]);
+    lisMenu.forEach( li => toggle(li, ['opacity-0','-translate-x']))
+}
+
+menu.onclick = toggleMenuMain
+closeMenu.onclick = toggleMenuMain
